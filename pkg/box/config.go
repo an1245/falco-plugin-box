9d2ed2c2c353cfb79ddb8702c607b0455256f9a0
an1245 <127995147+an1245@users.noreply.github.com>
2024-02-06 17:35:12 +1300

    update mandatory config items

--- pkg/box/config.go
+++ pkg/box/config.go
@@ -18,13 +18,13 @@ package box
 
 // PluginConfig represents a configuration of the GitHub plugin
 type PluginConfig struct {
-	BoxClientId       string `json:"boxclientid" jsonschema:"title=Box Custom App Client Id"`
-	BoxClientSecret   string `json:"boxclientsecret" jsonschema:"title=Box Custom App Client Secret"`
-	BoxEnterpriseID   int64  `json:"boxenterpriseid" jsonschema:"title=Box Enterprise ID"`
+	BoxClientId       string `json:"boxclientid" jsonschema:"title=Box Custom App Client Id,required"`
+	BoxClientSecret   string `json:"boxclientsecret" jsonschema:"title=Box Custom App Client Secret,required"`
+	BoxEnterpriseID   int64  `json:"boxenterpriseid" jsonschema:"title=Box Enterprise ID,required"`
 	MaxmindCityDBPath string `json:"maxmindcitydbpath" jsonschema:"title=Path to Maxmind GeoLite2 or GeoIP2 City Database"`
 	Debug             bool   `json:"Debug" jsonschema:"title=Enable debug output (true = yes, false=no)"`
 	DebugLevel        int    `json:"DebugLevel" jsonschema:"title=What Debug Level is set (0=INFO,)"`
-	PollIntervalSecs  int    `json:"PollIntervalSecs" jsonschema:"title=How often do you want to poll the API?"`
+	PollIntervalSecs  int    `json:"PollIntervalSecs" jsonschema:"title=How often do you want to poll the API?,required,default=300"`
 }
 
 // Reset sets the configuration to its default values
